78bab0ce902012110000e27a2be649d08476a4e3
GuntherWirgmanLevratto <96074862+GuntherWirgmanLevratto@users.noreply.github.com>
2025-02-07 16:04:42 -0300

    rollback on cluster DBR

--- RUNME.py
+++ RUNME.py
@@ -76,17 +76,16 @@ job_json = {
             {
                 "job_cluster_key": "aml_cluster",
                 "new_cluster": {
-                    "spark_version": "15.4.x-cpu-ml-scala2.12",
-                    "node_type_id": "Standard_D4ads_v5",
-                    "driver_node_type_id": "Standard_D4ads_v5",
-                    "num_workers": 2,
+                    "spark_version": "11.3.x-cpu-ml-scala2.12",
                     "spark_conf": {
                         "spark.databricks.delta.formatCheck.enabled": "false"
-                    },
+                        },
+                    "num_workers": 2,
+                    "node_type_id": {"AWS": "i3.xlarge", "MSA": "Standard_DS3_v2", "GCP": "n1-highmem-4"}, # different from standard API
                     "custom_tags": {
                         "usage": "solacc_automation",
                         "group": "FSI"
-                    }
+                    },
                 }
             }
         ]
